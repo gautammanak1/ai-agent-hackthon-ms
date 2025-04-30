3ce9d9144c7906d69e6dc15cd19f1ee987c2aac7
GAUTAM MANAK <106014185+gautammanak1@users.noreply.github.com>
2025-04-30 14:59:15 +0530

    fix the build

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,18 +1,18 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    // Ignore canvas in the browser
     if (!isServer) {
-      config.resolve.alias["canvas"] = false;
-      config.resolve.extensions = [".js", ".jsx", ".ts", ".tsx"];
+      config.resolve.fallback = {
+        fs: false,
+        canvas: false,
+      };
+
+      config.module.rules.push({
+        test: /canvas/,
+        use: 'ignore-loader',
+      });
     }
-
-    // Handle .node files
-    config.module.rules.push({
-      test: /\.node$/,
-      use: 'ignore-loader', // Alternative to null-loader
-    });
-
     return config;
   },
 };
